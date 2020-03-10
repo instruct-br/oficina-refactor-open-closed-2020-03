8c91439e182d4d05a1fd69cf7d0c60a9b65353c9
Oscar Esgalha <oscar@instruct.com.br>
2020-03-10 20:28:14 -0300

    Extract Function
    Move this code to a separate new function and
    replace the old code with a call to the method.

--- src/main.py
+++ src/main.py
@@ -8,13 +8,17 @@ def contains_python(text):
     return 'python' in text
 
 
+def contains_number(text):
+    return re.compile(r"\(?\d+\)?\s*\d+\-*\d+").search(text)
+
+
 def main(emails):
     valid_mails = []
     for email in emails:
         cv_txt = extract_text(f'../cvs/{email}.pdf')
         if not contains_python(cv_txt.lower()):
             continue
-        if not re.compile(r"\(?\d+\)?\s*\d+\-*\d+").search(cv_txt.lower()):
+        if not contains_number(cv_txt.lower()):
             continue
         valid_mails.append(email)
     return valid_mails
